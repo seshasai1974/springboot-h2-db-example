dd5692e80b6f132c7ca2fe0d3f361cf496b4b7de
Venkat Seshasai <venkat_seshasai@epam.com>
2022-08-08 20:46:53 +0530

    Initial commit : Sprinboot H2 Crud Example

---
+++ src/main/java/com/springboot/db/example/springbooth2dbexample/SpringbootH2DbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.db.example.springbooth2dbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootH2DbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootH2DbExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/db/example/springbooth2dbexample/SpringbootH2DbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.db.example.springbooth2dbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootH2DbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
